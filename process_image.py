11931a6cfb43e49777895741735e82ce4ce4a3dd
valentinrudloff <valentin.rudloff.perso@gmail.com>
2022-09-28 00:54:35 +0200

    update process image

--- process_image.py
+++ process_image.py
@@ -5,6 +5,7 @@ import rawpy
 import sys
 
 file = sys.argv[1]
+output_file = sys.argv[2]
 img = Image.open(file)
 with rawpy.imread(file) as raw:
     rgb = raw.postprocess(use_camera_wb=True)
@@ -17,5 +18,5 @@ filter = ImageEnhance.Sharpness(img)
 img = filter.enhance(2)
 filter = ImageEnhance.Color(img)
 img = filter.enhance(0.9)
-img.save(sys.argv[1].replace("dng", "jpg"), quality=100, subsampling=0)
-img.save(sys.argv[1].replace("dng", "tiff"))
+img.save(output_file.replace("dng", "jpg"), quality=90, subsampling=0)
+img.save(output_file.replace("dng", "tiff"))
