749149dcc0e0b2675b0b611c98b8367cb4378b7d
valentinrudloff <valentin.rudloff.perso@gmail.com>
2022-10-10 23:50:34 +0200

    add normalize

--- source/normalize/normalize.py
+++ source/normalize/normalize.py
@@ -1,10 +1,6 @@
-import jetson.utils
+import cv2
+import numpy as np
 
-# load the input image (its pixels will be in the range of 0-255)
-imgInput = jetson.utils.loadImage('/app/test/image.jpg')
-
-# allocate the output image, with the same dimensions as input
-imgOutput = jetson.utils.cudaAllocMapped(width=imgInput.width, height=imgInput.height, format=imgInput.format)
-
-# normalize the image from [0,255] to [0,1]
-jetson.utils.cudaNormalize(imgInput, (0,255), imgOutput, (0,1))
+image = cv2.imread("image.jpg")
+image_norm = cv2.normalize(image, None, 25, 255, cv2.NORM_MINMAX)
+cv2.imwrite("image_norm.jpg", image_norm)
