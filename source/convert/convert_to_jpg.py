4a8d71690195e4f685b1dde66b6ff397764ad938
valentinrudloff <valentin.rudloff.perso@gmail.com>
2022-10-11 22:31:16 +0200

    update convert

--- source/convert/convert_to_jpg.py
+++ source/convert/convert_to_jpg.py
@@ -1,10 +1,11 @@
+#!/usr/local/bin/python3
 import sys
 import rawpy
 from PIL import Image
 
+
 file = sys.argv[1]
-output_file = sys.argv[2]
 img = Image.open(file)
 with rawpy.imread(file) as raw:
     rgb = raw.postprocess(use_camera_wb=True)
-img.save(output_file.replace("dng", "jpg"), quality=90, subsampling=0)
+img.save(file.replace("dng", "jpg"), quality=90, subsampling=0)
